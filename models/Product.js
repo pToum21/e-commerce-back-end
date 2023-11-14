41afeec961a2a20bd279462ce329aa6aefb5e197
toum21 <pey2moo1@gmail.com>
2023-11-14 13:28:28 -0800

    defined the columns using sequilize inside of the models

--- models/Product.js
+++ models/Product.js
@@ -9,7 +9,21 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    product_name: {
+      type: DataTypes.STRING,
+    },
+    price: {
+      type: DataTypes.DECIMAL(10,2),
+      allowNull: false,
+    },
+    stock: {
+      type: DataTypes.INTEGER
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      
+    }
   },
   {
     sequelize,
