592d7f8f21e7f1e6ec1610022716ff8f88caa67b
toum21 <pey2moo1@gmail.com>
2023-11-14 18:19:17 -0800

    fixing null bug

--- models/index.js
+++ models/index.js
@@ -4,9 +4,14 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 // Products belongsTo Category
-Product.belongsTo(Category);
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE'
+});
 // Categories have many Products
-Category.hasMany(Product);
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+});
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: {
