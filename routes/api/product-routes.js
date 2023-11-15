84df5704fa8a519928fbc0221fbe0c7155ea5e78
toum21 <pey2moo1@gmail.com>
2023-11-15 13:52:23 -0800

    created category routes that allow the user to do CRUD methods

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,8 +4,11 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
+  const productData = await Product.findAll({
+    include: [Category, Tag]
+  });
   // be sure to include its associated Category and Tag data
 });
 
