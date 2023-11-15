d9b9bafdb9c9fdfbd333d4d652d5b513037d3115
toum21 <pey2moo1@gmail.com>
2023-11-14 20:11:39 -0800

    still working on relations

--- models/Product.js
+++ models/Product.js
@@ -18,17 +18,29 @@ Product.init(
     },
     product_name: {
       type: DataTypes.STRING,
+      allowNull: false,
     },
     price: {
       type: DataTypes.DECIMAL(10, 2),
       allowNull: false,
+      validate: {
+        isDecimal: true,
+      }
     },
     stock: {
-      type: DataTypes.INTEGER
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric: true
+      }
     },
     category_id: {
       type: DataTypes.INTEGER,
-      allowNull: false,
+      references: {
+        model: 'category',
+        key: 'id'
+      }
     }
   },
   {
